2799967eeb78b8c2ee1ab6da0a7b5b5e9cde202d
danemadsen <dane_madsen@hotmail.com>
2024-02-07 20:27:49 +1000

    remove redundant

--- lib/src/alpaca_format.dart
+++ lib/src/alpaca_format.dart
@@ -6,8 +6,4 @@ class AlpacaFormat extends PromptFormat {
             inputSequence: '### Input:',
             outputSequence: '### Response:',
             systemSequence: '### Instruction:');
-
-  String preparePrompt(String prompt) {
-    return '### Input:\n\n$prompt\n\n### Response:\n\n';
-  }
 }
